63132c615eb5465147682c0deb8e90c4f87d3639
OvernightSolo <sterlingtx75@gmail.com>
2022-12-08 22:54:03 -0600

    initial commit

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,16 @@
+const WorkoutDetails = ({ workout }) => {
+  return (
+    <div class="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (lbs): </strong> {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong> {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
