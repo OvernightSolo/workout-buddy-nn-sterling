77ecfdf53cc23f4b350e20163e52b818401ecac7
OvernightSolo <sterlingtx75@gmail.com>
2022-12-18 09:54:57 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ app.use("/api/workouts", workoutRoutes);
 
 // connect to db
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true",
+  })
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
