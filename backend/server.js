9f18b23ec45a005315b77052f0750cda49abd9fb
OvernightSolo <sterlingtx75@gmail.com>
2022-12-22 20:21:41 -0600

    update ability to deploy

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,7 @@ app.use("/api/workouts", workoutRoutes);
 
 // connect to db
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: "true",
-    useUnifiedTopology: "true",
-  })
+  .connect(process.env.MONGO_URI, {})
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
